03d3808832a04c3f08b0a82aab70835c36332a9a
MIkeCall1986 <34583657+MIkeCall1986@users.noreply.github.com>
2025-05-19 19:21:17 +0300

    Update parse_news.py

--- parse_news.py
+++ parse_news.py
@@ -26,7 +26,7 @@ SITES = [
 ]
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-OUTPUT_FILE = "docs/news.json"
+OUTPUT_FILE = "news.json"
 
 def parse_news():
     all_news = []
